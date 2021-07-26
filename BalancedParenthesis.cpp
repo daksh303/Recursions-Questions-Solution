75f149c6ed8365fddfcff205ee2dae3690239e37
daksh303 <76605122+daksh303@users.noreply.github.com>
2021-07-26 17:14:15 +0530

    Update BalancedParenthesis.cpp

--- BalancedParenthesis.cpp
+++ BalancedParenthesis.cpp
@@ -1,9 +1,5 @@
-#incude<iostream>
-#incude<stack>
-#incude<vector>
-#incude<string>
-using namespace std;
-void sove(int open,int cose ,string op,vector<string> &v) {
+
+void solve(int open,int cose ,string op,vector<string>& v) {
     if(open==0 && cose==0) {
         v.push_back(op);
         return;
@@ -12,33 +8,16 @@ void sove(int open,int cose ,string op,vector<string> &v) {
     string op1 = op;
     
     op1.push_back('(');
-    sove(open-1,cose,op1,v);
+    solve(open-1,cose,op1,v);
     return;
     }
     if(cose>open) 
     {
         string op2 = op;
         op2.push_back(')');
-        sove(open,cose-1,op2,v);
+        solve(open,cose-1,op2,v);
         return;
     }
 }
 
 
-int main() {
-    int n;
-    cin>>n;
-  
-
-     vector<string> v;
-    int open = n;
-    int cose = n;
-    string op = " ";
-    sove(open,cose,op,v);
-   for(int i = 0; i < v.size(); i++)
-	{
-		cout << v[i] << end;
-        
-    }
-    return 0;
-}
